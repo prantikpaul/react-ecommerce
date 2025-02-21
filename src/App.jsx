0d4983882ca02db0b1974f3143c244b874531d69
prantikpaul <pcp.3240@gmail.com>
2025-02-21 19:50:13 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <>
+      <div className='bg-red-800'>
+        pp 
+      </div>
+    </>
+  )
+}
+git config --global user.name "prantikpaul"
+git config --global user.email "pcp.3240@gmail.com"
